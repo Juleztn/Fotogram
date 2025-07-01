8e96a49d7d7e750423551ed4d2ba141c6ecc5df8
Jule Zieten <ju-zie@web.de>
2025-07-01 15:24:27 +0200

    img rendered on script, dialog added

--- script.js
+++ script.js
@@ -14,17 +14,34 @@ let images = [
     './img/zebra.jpg'
 ]
 
-let container = document.getElementById('image-container')
+let container = document.getElementById('image-container');
+let dialog = document.getElementById('image-big');
 
 function init() {
     showImages();
 }
 
 function showImages() {
-
     for (let i = 0; i < images.length; i++) {
-        let img = document.createElement('img');
-        img.src = images[i];
-        container.appendChild(img);
+        container.innerHTML += `<img class="album" onclick="toggleOverlay(); showDialog(${i})" src="${images[i]}" alt="">`;
     }
+}
+
+function toggleOverlay() {
+    let overlay = document.getElementById('overlay');
+    overlay.classList.toggle('d_none');
+}
+
+function showDialog(index) {
+    dialog.show(index);
+    dialog.innerHTML = `<div class="dialog-flex"><img class="image-dialog" src="${images[index]}" alt="">         
+            <div class="btn-left-right">
+                <img class="btn" src="./img/arrow-left-solid.svg">
+                <img class="btn" src="./img/arrow-right-solid.svg">
+            </div> 
+            </div>`;
+}
+
+function closeDialog() {
+    dialog.close();
 }
